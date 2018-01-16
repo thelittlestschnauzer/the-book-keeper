3fdf0c82fe8809d412e52502689c0b52bfa304a0
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2018-01-16 13:47:35 -0500

    added the login action get and post within user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,22 @@ class UsersController < ApplicationController
       redirect to "/booklists"
     end
   end
+
+  get "/login" do
+    if !logged_in?
+      erb :"/users/login"
+    else
+      redirect to "/booklists"
+    end
+  end
+
+  post "/login" do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/booklists"
+    else
+      redirect to "/signup"
+    end
+  end
 end
